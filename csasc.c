ddd624fb6e5d5d0b1129938501312cbe1790e09c
Arcelyth <arcelyth@tutamail.com>
2026-02-19 20:22:16 +0800

    add csasc

---
+++ csasc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+          /*                 #a&*.^&&e.        t_"o*/int       main(){int k,i   ,j,n;for(int k=
+          0;k<              4;k++)printf      ("%4s%4s%5s"     ,"Dec","Hex","   ");puts("");for
+         (i=0;             i<32      ;i++    ){for     (/*          **/               j=0
+        ;j<4;j++          ){int       n=i   +j*32;     printf       /*x               727
+       %4X     -&        !^*xz              x@@x        Bx(z)       K\&               ^sz
+      n,n      */(        "%4d%4X%5s",n,n   ,n<33                   ?/*               **/
+     (char*[]){"NUL"       ,"SOH","STX",    "ETX"                   ,/*               U*/
+    "EOT",     "ENQ",                "ACK"  ,"BEL"                  ,/*               ]*/
+   "BS",         "HT",               "LF"   ,"VT",                  //*               ,"D
+  "FF",           "CR",   "SO",     "SI"    ,"DLE"     ,"DC1"       ,//               ,"C
+ "DC2"            ,"DC3"  ,"DC4","NAK",      "SYN",   "ETB",  "CAN","EM","SUB"  ,"ESC","FS","GS"
+,"RS","US"     ,"SP"}[n]:    n==127?          "DEL":(char[]   ){n,0});}puts("") ;}}//A"&=".[\7*]
+
+      
