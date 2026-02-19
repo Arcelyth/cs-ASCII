c5807197781ba5e84be76198b93789c8e10af514
Arcelyth <arcelyth@tutamail.com>
2026-02-19 20:26:19 +0800

    add csasc

--- csasc.c
+++ csasc.c
@@ -6,9 +6,9 @@
         ;j<4;j++          ){int       n=i   +j*32;     printf       /*x               727
        %4X     -&        !^*xz              x@@x        Bx(z)       K\&               ^sz
       n,n      */(        "%4d%4X%5s",n,n   ,n<33                   ?/*               **/
-     (char*[]){"NUL"       ,"SOH","STX",    "ETX"                   ,/*               U*/
+     (char*[]){"NUL"       ,"SOH","STX",    "ETX"                   /*^               */,
     "EOT",     "ENQ",                "ACK"  ,"BEL"                  ,/*               ]*/
-   "BS",         "HT",               "LF"   ,"VT",                  //*               ,"D
+   "BS",         "HT",               "LF"   ,"VT",                  //&               "D@
   "FF",           "CR",   "SO",     "SI"    ,"DLE"     ,"DC1"       ,//               ,"C
  "DC2"            ,"DC3"  ,"DC4","NAK",      "SYN",   "ETB",  "CAN","EM","SUB"  ,"ESC","FS","GS"
 ,"RS","US"     ,"SP"}[n]:    n==127?          "DEL":(char[]   ){n,0});}puts("") ;}}//A"&=".[\7*]
